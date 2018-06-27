6816d62cfa9e7aa7147527e40cddcc8743b17dae
darkobits <joshua@frontlawn.net>
2018-06-27 00:15:59 -0700

    feat: Add env.

---
+++ src/env.spec.ts
@@ -0,0 +1,170 @@
+describe('env', () => {
+  let env: Function;
+
+  beforeEach(() => {
+    jest.resetModules();
+    env = require('./env').default; // tslint:disable-line no-require-imports
+  });
+
+  describe('when provided a non-string argument', () => {
+    it('should throw an error', () => {
+      expect(() => {
+        env(null);
+      }).toThrow('[env] Expected first argument to be of type "string"');
+    });
+  });
+
+  describe('when "process" is not defined', () => {
+    let ORIG_PROCESS: NodeJS.Process;
+
+    beforeEach(() => {
+      ORIG_PROCESS = process;
+      Reflect.deleteProperty(global, 'process');
+    });
+
+    it('should throw an error', () => {
+      expect.assertions(1);
+
+      try {
+        env('foo');
+      } catch (err) {
+        expect(err.message).toBe('[env] Global "process" does not exist.');
+      }
+    });
+
+    afterEach(() => {
+      global.process = ORIG_PROCESS;
+    });
+  });
+
+  describe('when "process" is a non-object', () => {
+    let ORIG_PROCESS: NodeJS.Process;
+
+    beforeEach(() => {
+      ORIG_PROCESS = process;
+      // @ts-ignore
+      process = true;
+    });
+
+    it('should throw an error', () => {
+      expect.assertions(1);
+
+      try {
+        env('foo');
+      } catch (err) {
+        expect(err.message).toMatch('[env] Expected "process" to be of type "object"');
+      }
+    });
+
+    afterEach(() => {
+      global.process = ORIG_PROCESS;
+    });
+  });
+
+  describe('when "process.env" is not defined', () => {
+    let ORIG_ENV: NodeJS.ProcessEnv;
+
+    beforeEach(() => {
+      ORIG_ENV = process.env;
+      Reflect.deleteProperty(process, 'env');
+    });
+
+    it('should throw an error', () => {
+      expect(() => {
+        env('foo');
+      }).toThrow('[env] "env" does not exist in object "process".');
+    });
+
+    afterEach(() => {
+      process.env = ORIG_ENV;
+    });
+  });
+
+  describe('when "process.env" is a non-object', () => {
+    let ORIG_ENV: NodeJS.ProcessEnv;
+
+    beforeEach(() => {
+      ORIG_ENV = process.env;
+      // @ts-ignore
+      process.env = true;
+    });
+
+    it('should throw an error', () => {
+      expect(() => {
+        env('foo');
+      }).toThrow('[env] Expected "process.env" to be of type "object"');
+    });
+
+    afterEach(() => {
+      process.env = ORIG_ENV;
+    });
+  });
+
+  describe('when strict is true and the requested variable is not defined', () => {
+    it('should throw an error', () => {
+      const variableName = '___UNDEFINED___';
+      expect(() => {
+        env(variableName, true);
+      }).toThrow(`[env] (Strict) "${variableName}" does not exist in object "process.env".`);
+    });
+  });
+
+  describe('when the requested variable is a number-like string', () => {
+    it('should return a number', () => {
+      const key = '__NUMBER_TEST__';
+      const value = '3.14';
+      const parsedValue = Number(value);
+
+      process.env[key] = value;
+
+      expect(env(key)).toBe(parsedValue);
+    });
+  });
+
+  describe('when the requested variable is the string "true"', () => {
+    it('should return the boolean value true', () => {
+      const key = '__TRUE_TEST__';
+      const value = 'true';
+
+      process.env[key] = value;
+
+      expect(env(key)).toBe(true);
+    });
+  });
+
+  describe('when the requested variable is the string "false"', () => {
+    it('should return the boolean value false', () => {
+      const key = '__FALSE_TEST__';
+      const value = 'false';
+
+      process.env[key] = value;
+
+      expect(env(key)).toBe(false);
+    });
+  });
+
+  describe('when the requested variable is a serialized JSON blob', () => {
+    it('should return the deserialized value', () => {
+      const key = '__JSON_TEST__';
+      const value = {
+        foo: 'bar',
+        baz: 'qux'
+      };
+
+      process.env[key] = JSON.stringify(value);
+
+      expect(env(key)).toMatchObject(value);
+    });
+  });
+
+  describe('when the requested variable is a generic string', () => {
+    it('should return the requested variable', () => {
+      const key = '__STRING_TEST__';
+      const value = 'kittens';
+
+      process.env[key] = value;
+
+      expect(env(key)).toBe(value);
+    });
+  });
+});
---
+++ src/env.ts
@@ -0,0 +1,53 @@
+export default function env(variableName: string, strict: boolean = false): any {
+  // Throw if first argument is not a string. Although normal objects may have
+  // non-string keys (such as Symbols) process.env may not.
+  if (typeof variableName !== 'string') {
+    throw new TypeError(`[env] Expected first argument to be of type "string", got "${typeof variableName}".`);
+  }
+
+  // Throw if 'process' does not exist. (User might be in the browser.)
+  if (typeof process === 'undefined') { // tslint:disable-line no-typeof-undefined
+    throw new Error('[env] Global "process" does not exist.');
+  }
+
+  // Throw if process is a non-object.
+  if (typeof process !== 'object') {
+    throw new TypeError(`[env] Expected "process" to be of type "object", got "${typeof process}".`);
+  }
+
+  // Throw if 'process.env' does not exist.
+  if (!Reflect.has(process, 'env')) {
+    throw new Error('[env] "env" does not exist in object "process".');
+  }
+
+  // Throw if process.env is a non-object.
+  if (typeof process.env !== 'object') {
+    throw new TypeError(`[env] Expected "process.env" to be of type "object", got "${typeof process.env}".`);
+  }
+
+  // Throw if in strict mode and the requested variable name does not exist.
+  if (strict && !Reflect.has(process.env, variableName)) {
+    throw new Error(`[env] (Strict) "${variableName}" does not exist in object "process.env".`);
+  }
+
+  try {
+    /**
+     * If the requested variable is JSON-parse-able, parse it and return it.
+     *
+     * This will properly convert:
+     * - Number-like string values to numbers.
+     * - The string value 'true' to the boolean value true.
+     * - The string value 'false' to the boolean false.
+     * - Any serialized data structures to their deserialized value.
+     */
+
+    // @ts-ignore
+    return JSON.parse(process.env[variableName]);
+  } catch (err) {
+    /**
+     * If JSON.parse fails, return the value. This will be the case for most
+     * string literal values.
+     */
+    return process.env[variableName];
+  }
+}
